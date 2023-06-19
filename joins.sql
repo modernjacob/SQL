d4b7ef29b8ba75ab3f0eddc4e863331843d49064
Jacob Huckleberry <79178033+modernjacob@users.noreply.github.com>
2023-06-19 16:51:21 -0700

    Update joins.sql

--- joins.sql
+++ joins.sql
@@ -21,3 +21,16 @@ FROM
 LEFT JOIN
   products p ON c.category_id = p.category_id
 WHERE product_id IS NULL;
+
+
+-- Join with Group By
+
+SELECT
+  c.email_address,
+  COUNT(o.customer_id) AS product_count
+FROM 
+  customers c
+JOIN
+  orders o ON c.customers_id = o.customers_id
+GROUP BY c.email_address
+HAVING product_count > 1;
