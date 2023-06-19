c684331e46f115f9c5e30e088e16c5b431e3f0db
Jacob Huckleberry <79178033+modernjacob@users.noreply.github.com>
2023-06-19 16:43:47 -0700

    Create inserts.sql

---
+++ inserts.sql
@@ -0,0 +1,8 @@
+-- Insert into the customers table
+
+INSERT INTO customers (
+  customer_id, email_address, password, first_name, last_name
+)
+VALUES (
+  DEFAULT, 'nik@nikithy.com', '', 'Nik', 'Nikithy'
+);
