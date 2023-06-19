5ded445b86107b216044f081d85d818aef5b9dd4
Jacob Huckleberry <79178033+modernjacob@users.noreply.github.com>
2023-06-19 16:29:14 -0700

    Update calculations.sql

--- calculations.sql
+++ calculations.sql
@@ -1,4 +1,4 @@
---Creating new columns for discount amounts and pricing
+-- Creating new columns for discount amounts and pricing
 
 SELECT
   product name,
@@ -10,3 +10,19 @@ FROM
   products
 ORDER BY discount_price DESC
 LIMIT 5;
+
+
+-- Calculating totals with quantity
+
+SELECT
+  iten_id,
+  item_price,
+  discount_amount,
+  quantity,
+  item_price * quantity AS price_total,
+  discount_amount * quantity AS discount_total,
+  (item_price - discount_amount) * quantity AS item_total
+ FROM
+  order_items
+HAVING item_total > 500
+ORDER BY item_total DESC;
